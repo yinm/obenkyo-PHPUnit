6783cd741dd3e8cfe8f2c7f5e65c4145e058f746
yinm <yinm.cminor9@gmail.com>
2017-06-18 13:48:10 +0900

    dataProvider

--- test.php
+++ test.php
@@ -1,29 +1,24 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class MultipleDependenciesTest extends TestCase
+class DataTest extends TestCase
 {
-    public function testProducerFirst()
-    {
-        $this->assertTrue(true);
-        return 'first';
-    }
-
-    public function testProducerSecond()
-    {
-        $this->assertTrue(true);
-        return 'second';
-    }
-
     /**
-     * @depends testProducerFirst
-     * @depends testProducerSecond
+     * @dataProvider additionProvider
      */
-    public function testConsumer()
+    public function testAdd($a, $b, $expected)
     {
-        $this->assertEquals(
-            ['first', 'second'],
-            func_get_args()
-        );
+        $this->assertEquals($expected, $a + $b);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            [0, 0, 0],
+            [0, 1, 1],
+            [1, 0, 1],
+            [1, 1, 3]
+        ];
     }
 }
+
