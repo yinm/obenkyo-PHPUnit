d65d6a268815ae0dcf9b5429fde57d546537ecab
yinm <yinm.cminor9@gmail.com>
2017-06-18 13:22:19 +0900

    @depends アノテーションを使った、依存性の表現

--- test.php
+++ test.php
@@ -3,16 +3,32 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
-    public function testPushAndPop()
+    public function testEmpty()
     {
         $stack = [];
-        $this->assertEquals(0, count($stack));
+        $this->assertEmpty($stack);
 
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(array $stack)
+    {
         array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
+        $this->assertEquals('foo', $stack[count($stack) - 1]);
+        $this->assertNotEmpty($stack);
 
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     */
+    public function testPop(array $stack)
+    {
         $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
+        $this->assertEmpty($stack);
     }
 }
