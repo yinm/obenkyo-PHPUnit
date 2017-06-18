3d5e1fcb3cc600eea0c862d7b78ea142cd92c219
yinm <yinm.cminor9@gmail.com>
2017-06-18 14:37:55 +0900

    PHPUnitのエラーは、可能な限り差異を表現してくれる

--- test.php
+++ test.php
@@ -1,17 +1,12 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class OutputTest extends TestCase
+class ArrayDiffTest extends TestCase
 {
-    public function testExceptFooActualFoo()
-    {
-        $this->expectOutputString('foo');
-        print 'foo';
-    }
-
-    public function testExpectBarActualBaz()
-    {
-        $this->expectOutputString('bar');
-        print 'baz';
+    public function testEquality() {
+        $this->assertEquals(
+            [1, 2,  3, 4, 5, 6],
+            [1, 2, 33, 4, 5, 6]
+        );
     }
 }
