67958fe775ffd86132fb15b040a66d16b5c4327f
yinm <yinm.cminor9@gmail.com>
2017-06-18 14:24:26 +0900

    @expectedException

--- test.php
+++ test.php
@@ -3,9 +3,11 @@ use PHPUnit\Framework\TestCase;
 
 class ExceptionTest extends TestCase
 {
+    /**
+     * @expectedException InvalidArgumentException
+     */
     public function testException()
     {
-        $this->expectException(InvalidArgumentException::class);
     }
 }
 
