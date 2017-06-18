3d746c43524bf2285efe253620c234c737243be4
yinm <yinm.cminor9@gmail.com>
2017-06-18 14:45:38 +0900

    assertSameで完璧に合わせた場合

--- test.php
+++ test.php
@@ -4,9 +4,9 @@ use PHPUnit\Framework\TestCase;
 class ArrayWeakComparisonTest extends TestCase
 {
     public function testEquality() {
-        $this->assertEquals(
+        $this->assertSame(
             [1, 2, 3, 4, 5, 6],
-            ['1', 2, 33, 4, 5, 6]
+            [1, 2, 3, 4, 5, 6]
         );
     }
 }
