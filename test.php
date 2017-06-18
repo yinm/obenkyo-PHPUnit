3fafe6de42b113080412ef29e17c1c94751b5aae
yinm <yinm.cminor9@gmail.com>
2017-06-18 13:51:23 +0900

    配列のキーにテスト内容を入れるとわかりやすい

--- test.php
+++ test.php
@@ -4,20 +4,20 @@ use PHPUnit\Framework\TestCase;
 class DataTest extends TestCase
 {
     /**
-     * @dataProvider additionProvider
+     * @dataProvider addtionProvider
      */
     public function testAdd($a, $b, $expected)
     {
         $this->assertEquals($expected, $a + $b);
     }
 
-    public function additionProvider()
+    public function addtionProvider()
     {
         return [
-            [0, 0, 0],
-            [0, 1, 1],
-            [1, 0, 1],
-            [1, 1, 3]
+            'adding zeros'  => [0, 0, 0],
+            'zero plus one' => [0, 1, 1],
+            'one plus zero' => [1, 0, 1],
+            'one plus one'  => [1, 1, 3]
         ];
     }
 }
