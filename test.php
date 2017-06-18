12631224b03a2b766e0547e5e8802b4da8cd2cb1
yinm <yinm.cminor9@gmail.com>
2017-06-18 14:02:24 +0900

    Iteratorオブジェクトを使う

--- test.php
+++ test.php
@@ -1,24 +1,20 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+require 'CsvFileIterator.php';
+
 class DataTest extends TestCase
 {
     /**
-     * @dataProvider addtionProvider
+     * @dataProvider additionProvider
      */
     public function testAdd($a, $b, $expected)
     {
         $this->assertEquals($expected, $a + $b);
     }
 
-    public function addtionProvider()
+    public function additionProvider()
     {
-        return [
-            'adding zeros'  => [0, 0, 0],
-            'zero plus one' => [0, 1, 1],
-            'one plus zero' => [1, 0, 1],
-            'one plus one'  => [1, 1, 3]
-        ];
+        return new CsvFileIterator('data.csv');
     }
 }
-
