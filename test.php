2216616758d022e044b572f496af8ead19fe89d9
yinm <yinm.cminor9@gmail.com>
2017-06-18 14:22:50 +0900

    expectException

--- test.php
+++ test.php
@@ -1,35 +1,11 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class DependencyAndDataProviderComboTest extends TestCase
+class ExceptionTest extends TestCase
 {
-    public function provider()
+    public function testException()
     {
-        return [['provider1'], ['provider2']];
-    }
-
-    public function testProducerFirst()
-    {
-        $this->assertTrue(true);
-        return 'first';
-    }
-
-    public function testProducerSecond()
-    {
-        $this->assertTrue(true);
-        return 'second';
-    }
-
-    /**
-     * @depends testProducerFirst
-     * @depends testProducerSecond
-     * @dataProvider provider
-     */
-    public function testConsumer()
-    {
-        $this->assertEquals(
-            ['provider1', 'first', 'second'],
-            func_get_args()
-        );
+        $this->expectException(InvalidArgumentException::class);
     }
 }
+
