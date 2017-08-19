0a92c1fc373d97dce4da5ef16d0b95cd5e292291
yinm <yinm.cminor9@gmail.com>
2017-08-19 14:59:46 +0900

    add expected error tests

---
+++ ch2/ExpectedErrorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExpectedErrorTest extends TestCase
+{
+    /**
+     * @expectedException PHPUnit\Framework\Error\Error
+     */
+    public function testFailingInclude()
+    {
+        include 'not_existing_file.php';
+    }
+}
