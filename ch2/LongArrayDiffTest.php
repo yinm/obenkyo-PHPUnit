b21a5696383407b55066f409a138758140a9a18b
yinm <yinm.cminor9@gmail.com>
2017-08-19 15:23:19 +0900

    add long array diff test

---
+++ ch2/LongArrayDiffTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class LongArrayDiffTest extends TestCase
+{
+    public function testEquality()
+    {
+        $this->assertSame(
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2,  3, 4, 5, 6],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 33, 4, 5, 6]
+        );
+    }
+}
