73b50e916f8712f14d5834466b8972839a8cb53f
yinm <yinm.cminor9@gmail.com>
2017-08-20 10:55:52 +0900

    fix

--- ch9/StubTest5.php
+++ ch9/StubTest5.php
@@ -6,8 +6,13 @@ class StubTest5 extends TestCase
 {
     public function testReturnValueMapStub()
     {
-        $stub = $this->createMock(SomeClass::class);
+//        $stub = $this->createMock(SomeClass::class);
+        $stub = $this->getMockBuilder('SomeClass')
+                     ->setMethods(['doSomething'])
+                     ->getMock();
 
+        // 引数を配列で指定する。配列の最後の値は返り値になる
+        // ref: http://qiita.com/putan/items/d40907545e1fbb1ad4aa
         $map = [
             ['a', 'b', 'c', 'd'],
             ['e', 'f', 'g', 'h']
