f89131464ba5d93acc6c4e244358181fed12c2e5
yinm <yinm.cminor9@gmail.com>
2017-08-20 09:48:58 +0900

    fix

--- ch9/StubTest4.php
+++ ch9/StubTest4.php
@@ -6,7 +6,10 @@ class StubTest4 extends TestCase
 {
     public function testReturnSelf()
     {
-        $stub = $this->createMock(SomeClass::class);
+//        $stub = $this->createMock(SomeClass::class);
+        $stub = $this->getMockBuilder('SomeClass')
+                     ->setMethods(['doSomething'])
+                     ->getMock();
 
         $stub->method('doSomething')
              ->will($this->returnSelf());
